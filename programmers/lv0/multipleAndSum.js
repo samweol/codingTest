bcffc7666bf08b004be5e2fb96fc7d7de60f0bc4
joo <sejuyes@gmail.com>
2023-05-02 15:01:06 +0900

    first commit

---
+++ programmers/lv0/multipleAndSum.js
@@ -0,0 +1,14 @@
+// Q. 정수가 담긴 리스트 num_list가 주어질 때, 모든 원소들의 곱이 모든 원소들의 합의 제곱보다 작으면 1을 크면 0을 return하도록 solution 함수를 완성해주세요.
+
+function solution(num_list) {
+  const multipleSum = num_list.reduce((acc, cur) => acc * cur, 1);
+  const sum = Math.pow(
+    num_list.reduce((acc, cur) => acc + cur, 0),
+    2
+  );
+  let answer = 0;
+  if (multipleSum < sum) {
+    answer = 1;
+  }
+  return answer;
+}
