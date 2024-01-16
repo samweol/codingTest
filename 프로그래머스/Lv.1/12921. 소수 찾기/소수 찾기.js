6841d99f8355684a5bf2ff332f968303d69e6718
samweol <94788360+samweol@users.noreply.github.com>
2024-01-16 21:39:01 +0900

    [level Lv.1] Title: 소수 찾기, Time: 50.86 ms, Memory: 44.9 MB -BaekjoonHub

--- 프로그래머스/Lv.1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/Lv.1/12921. 소수 찾기/소수 찾기.js
@@ -1,15 +1,15 @@
 function solution(n) {
-    const array = Array(n+1).fill(0).map((_,idx)=>idx)
-    for(let i=2;i<=Math.sqrt(n-1);i++){
-        for(let j=i*2;j<array.length;j+=i){
-            if(array[j] === 0){
-                continue
-            }
-            if(array[j] % i === 0){
-                array[j] = 0
-            }
+    let array = Array(n+1).fill(true);
+    array[0] = array[1] = false; // 0, 1 은 제외하고 가야하므로
+    
+    // 2부터 N의 제곱근까지 반복해야하므로 반복문 사용
+    for(let i=2; i<=Math.sqrt(n); i++) {
+        if(array[i] === false) continue;
+        
+        for(let j = i * i; j<=n; j+=i) {
+            array[j] = false;
         }
     }
-
-    return array.filter(v=>v!==0).length - 1
+    
+   return array.filter(item => item).length;
 }
