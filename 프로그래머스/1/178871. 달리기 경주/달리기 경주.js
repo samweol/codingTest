7fad0164ce9af4f3f99c3f1579ac3312ca9d74fc
samweol <94788360+samweol@users.noreply.github.com>
2024-02-08 23:04:56 +0900

    [level 1] Title: 달리기 경주, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -1,19 +1,19 @@
 function solution(players, callings) {
-    const hash = new Map();
-    
-    players.forEach((name, index) => {
-        hash.set(name, index);
+    let playerObj = {};
+    players.forEach((item, idx) => {
+        playerObj[item] = idx;
     })
-    
-    callings.forEach(name => {
-        const currIdx = hash.get(name);
-        const front = players[currIdx - 1];
-
-        [players[currIdx], players[currIdx -1]] = [players[currIdx -1], players[currIdx]];
+    for(let i=0; i<callings.length; i++) {
+        const currentPosition = playerObj[callings[i]];
+        let front = players[currentPosition - 1];
+        let current = players[currentPosition];
+        let temp = "";
+        players[currentPosition - 1] = current;
+        players[currentPosition] = front
         
-        hash.set(name, hash.get(name) - 1);
-        hash.set(front, hash.get(name) + 1);
-    })
+        playerObj[current] = playerObj[current] - 1;
+        playerObj[front] = playerObj[front] + 1;
+    }
     
-    return players;
+    return players
 }
