be5b59100b0852d120603ce213f11f77a4253ea9
samweol <94788360+samweol@users.noreply.github.com>
2024-02-08 21:40:40 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.64 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,24 +1,26 @@
-const changeToDate = (year, month, date) => {
-  return +year * 12 * 28 + +month * 28 + +date;
-};
-
 function solution(today, terms, privacies) {
-  var answer = [];
-
-  const termsObj = {};
-  for (const info of terms) {
-    const [type, month] = info.split(' ');
-    termsObj[type] = changeToDate(0, month, 0);
-  }
-
-  const todayDate = changeToDate(...today.split('.'));
-
-  for (let i = 0; i < privacies.length; i++) {
-    const [date, type] = privacies[i].split(' ');
-    const dueDate = changeToDate(...date.split('.')) + termsObj[type] - 1;
-
-    if (todayDate > dueDate) answer.push(i + 1);
-  }
-
-  return answer;
+    let termObj = {};
+    terms.forEach(item => {
+        const [term, month] = item.split(" ");
+        termObj[term] = month;
+    })
+    // {"A":"6","B":"12","C":"3"}
+    
+    let result = [];
+    privacies.forEach((item, idx) => {
+        const [date, term] = item.split(" ");
+        const [year, month, day] = date.split(".");
+        
+        const timestamp = +day + +month*28 + +year*12*28 + +termObj[term]*28;
+        
+        const [todayYear, todayMonth, todayDay] = today.split(".");
+        const todaystamp = +todayDay+ +todayMonth*28 + +todayYear*12*28
+        
+        if(todaystamp >= timestamp) {
+            result.push(idx + 1)
+        }
+        
+    })
+    
+    return result
 }
