e4f6ee8c9d1305a29cdb15ba33071a7f0326a4f3
samweol <94788360+samweol@users.noreply.github.com>
2024-02-23 16:57:38 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.73 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,23 +1,12 @@
 function solution(A,B){
-    const tempA = A;
-    const tempB = B;
-    
-    tempA.sort((a, b) => a - b);
-    tempB.sort((a, b) => b - a);
-    
-    let tempSum = 0;
-    for(let i=0; i<tempA.length; i++) {
-        tempSum += tempA[i] * tempB[i]
-    }
-    
     A.sort((a, b) => b - a);
     B.sort((a, b) => a - b);
     
     let sum = 0;
-    for(let i=0; i<tempA.length; i++) {
-        sum += tempA[i] * tempB[i]
+    for(let i=0; i<A.length; i++) {
+        sum += A[i] * B[i]
     }
     
-    return Math.min(tempSum, sum)
+    return sum
     
 }
