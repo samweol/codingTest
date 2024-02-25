0b41669bb631d80d0b92dc7c97af0c52267ad0f2
samweol <94788360+samweol@users.noreply.github.com>
2024-02-25 20:46:37 +0900

    [level 2] Title: 올바른 괄호, Time: 4.95 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,16 +1,22 @@
-function solution(s) {
-    let openBrackets = 0;
-
-    for(let i = 0; i < s.length; i++) {
-        if(s[i] === '(') {
-            openBrackets++;
-        } else if(s[i] === ')') {
-            openBrackets--;
-            if(openBrackets < 0) {
-                return false;
+function solution(s){
+    const sArr = s.split("");
+    const checkArr = [];
+    
+    // 시작이 )로 시작하면 무조건 잘못된 괄호이기때문에
+    if(sArr[0] === ")" || sArr[sArr.length - 1] === "(") {
+        return false
+    }
+    
+    for(let i =0; i<sArr.length; i++) {
+        if(sArr[i] === "(") {
+            checkArr.push(sArr[i]);
+        } else {
+            const item = checkArr.pop();
+            if(item !== "(") {
+                return false
             }
         }
     }
-
-    return openBrackets === 0;
+    
+    return checkArr.length > 0 ? false : true
 }
